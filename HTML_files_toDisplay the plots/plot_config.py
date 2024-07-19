c8421622168499f959400fef58894e253d7f0752
ateeq-khaliq <154272206+ateeq-khaliq@users.noreply.github.com>
2024-07-19 10:53:08 -0400

    Add files via upload

---
+++ HTML_files_toDisplay the plots/plot_config.py
@@ -0,0 +1,23 @@
+# plot_config.py
+
+plot_files = [
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/1_Plot_meta.pdf",
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/2_feature_plot2.pdf",
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/3_split_umap_by_metaprogram.pdf",
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/4_MP_correlation_heatmap_with_clustering.pdf",
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/5_MP_Analysis_Revised_Final_treated_Donut_BArplot_Sperman.pdf",
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/6_pathway_enrichment_all_MPs_Hallmark.pdf",
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/7_pathway_enrichment_all_MPs_Go_BP.pdf",
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/8_pathway_enrichment_all_MPs_KEGG.pdf",
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/9_pathway_enrichment_all_MPs_REACTOME.pdf",
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/10_correlation_heatmap_enhanced.png",
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/11_correlation_corrplot.png",
+    "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/12_hypergeometric_test_plot.png",
+]
+
+output_dir = "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/png_plots"
+output_html = "/Users/akhaliq/Desktop/Gene_NMf/Exploratory_analysis/MP_plots_summary_test.html"
+
+title = "MetaPrograms (NMF) Exploratory Analysis"
+lab_name = "Masood Lab"
+proprietary_text = "Confidential"
